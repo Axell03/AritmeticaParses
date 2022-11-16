593f29fa05a384961b19f04488a5cb96b77b10ca
Jinn Woo <103296399+Axell03@users.noreply.github.com>
2022-11-16 17:19:53 -0400

    Parses

---
+++ parser/Program.cs
@@ -0,0 +1,14 @@
+﻿using Antlr4.Runtime;
+using parser;
+
+string input = "5*6+9*3-1";
+Console.WriteLine($"Input: {input}\n");
+
+ICharStream stream = CharStreams.fromString(input);
+A_Lexer lexer = new A_Lexer(stream);
+CommonTokenStream tokens = new CommonTokenStream(lexer);
+A_Parser parser = new aritmeticaParser(tokens);
+var tree = parser.aritmetica();
+
+Aritmetica aritmetica = new Aritmetica();
+int res = aritmetica.Visit(tree);
